0e1b5cf59fabd16b153a3ecd18c64cbd658ab07a
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-08 13:17:54 +0000

    Add Post component

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,12 @@
 </template>
 
 <script> 
+import Post from './components/Post'
 export default {
   name: 'App', 
+  components: {
+    Post
+  }
 }
 </script>
 
---
+++ src/components/Post.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Hello Guys, This Adibe </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Post'
+}
+</script>
