6cc8d471508d9e2051a78445eff7171d1944b124
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-08 23:23:40 +0000

    add navbar compo

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav>
+            <md-avatar>
+                <img src="" alt="">
+            </md-avatar>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import {MdAvatar} from 'vue-material/dist/components'
+Vue.use(MdAvatar)
+
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+
+</style>
