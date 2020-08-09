553bf3daa4e1a7201f1dbfe319573e4bcb5d1c96
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-09 23:24:36 +0000

    Add styled stories

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -2,7 +2,7 @@
   <div id="story">
     <img
       src="https://scontent.fcmn5-1.fna.fbcdn.net/v/t1.0-9/41678515_327688411138792_3693402459851456512_o.jpg?_nc_cat=102&_nc_sid=8bfeb9&_nc_ohc=xVOfFBP42Q8AX-AHuOp&_nc_ht=scontent.fcmn5-1.fna&oh=df93a02ba478cf9708ded327db7f59bb&oe=5F5437EA"
-      alt
+      alt=""
       class="story__mediaImg"
     />
     <div class="story__userImg">
@@ -37,7 +37,7 @@ export default {
   height: 230px;
   max-height: 286px;
   width: 24%;
-  background: palevioletred;
+  background: black;
   border-radius: 10px;
   margin: 0 4px;
   position: relative;
@@ -45,15 +45,28 @@ export default {
 }
 .story__mediaImg {
   border-radius: 10px;
+  height: 100%;
+  transition: 0.1s all;
+}
+.story__mediaImg:hover {
+  opacity: 0.8;
 }
 .story__userImg {
   position: absolute;
-  top: 0;
+  top: 3%;
+  left: 3%;
   z-index: 99;
-  border: 3px solid white;
+
   border-radius: 100%;
-  height: 40px;
-  width: 40px;
+  height: 45px;
+  width: 45px;
+  padding: 0;
+}
+.story__userImg img {
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  border: 4px solid #1876f2;;
 }
 .story__userName {
   position: absolute;
