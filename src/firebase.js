8ebfeb6ed17d93837fcb8909c813c7d8a63b1e28
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-09 23:51:01 +0000

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCoJ7u54x-sF951PrEcnmiW9JOARbYUj6M",
+    authDomain: "facebook-clone-16007.firebaseapp.com",
+    databaseURL: "https://facebook-clone-16007.firebaseio.com",
+    projectId: "facebook-clone-16007",
+    storageBucket: "facebook-clone-16007.appspot.com",
+    messagingSenderId: "148797229644",
+    appId: "1:148797229644:web:55862a460b9c46ac5482e9",
+    measurementId: "G-88JHMHX0QS"
+  };
