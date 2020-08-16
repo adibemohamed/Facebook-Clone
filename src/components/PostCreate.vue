52b4348f8dd0f791671209af7022ff6ba4d3bff6
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-16 23:01:06 +0000

    cleaning

--- src/components/PostCreate.vue
+++ src/components/PostCreate.vue
@@ -91,10 +91,9 @@ export default {
   name: "PostCreate",
   data: function() {
     return {
-      userImgUrl: "https://scontent.fcmn5-1.fna.fbcdn.net/v/t1.0-1/p200x200/87942170_623106144930349_6672718284065865728_n.jpg?_nc_cat=108&_nc_sid=7206a8&_nc_ohc=zwVpFzJUkcoAX-sR_yJ&_nc_ht=scontent.fcmn5-1.fna&_nc_tp=6&oh=ddbfec275df23073c4d5d3a94dbf1260&oe=5F5518E8",
       showDialog: false,
-      icon: "../assets/facebook-clone.png",
-      firstName: "Mohamed",
+      username: null,
+      timetamp: null,
       caption: null,
       media: null
     };
